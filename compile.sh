32f054d4e5309b2549b67bdb9c1784d5eb68d34b
Robert Hambrock <roberthambrock@gmail.com>
2021-12-12 22:04:22 +0100

    switch to compile.sh for native image compilation

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+"native-image" \
+    -cp "$(clojure -Spath):classes" \
+    -H:Name=parallel-offer-taker \
+    -H:+ReportExceptionStackTraces \
+    --initialize-at-build-time  \
+    --enable-https \
+    --verbose \
+    --no-server \
+    "-J-Xmx3g" \
+    parallel_offer_taker.core
