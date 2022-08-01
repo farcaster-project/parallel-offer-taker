faf8360711d9b325417537cdc6ba19bc430c0ae8
Robert Hambrock <roberthambrock@gmail.com>
2022-08-01 16:25:46 +0200

    use external config.edn file

--- src/parallel_offer_taker/core.clj
+++ src/parallel_offer_taker/core.clj
@@ -1,9 +1,10 @@
 (ns parallel-offer-taker.core
   (:require
    [clj-http.lite.client :as http]
-   [clojure.data.json :as json]
-   [clojure.java.shell]
    clojure.core
+   [clojure.data.json :as json]
+   clojure.edn
+   [clojure.java.shell]
    clojure.set)
   (:gen-class)
   )
@@ -21,27 +22,24 @@
   (reset! offers (offers-get)))
 
 (def offers-cached (atom @offers))
-(count (clojure.set/difference (into #{} @offers) (into #{} @offers-cached)))
-(count (clojure.set/difference (into #{} @offers-cached) (into #{} @offers)))
+(comment
+  (count (clojure.set/difference (into #{} @offers) (into #{} @offers-cached)))
+  (count (clojure.set/difference (into #{} @offers-cached) (into #{} @offers))))
 
-(def master-data-dir "***REMOVED***")
-
-;; (def farcaster-stash-btc "***REMOVED***")
-(def farcaster-stash-btc "***REMOVED***")
-;; (def farcaster-stash-xmr "***REMOVED***")
-(def farcaster-stash-xmr "***REMOVED***")
+(defn read-config [path]
+  (clojure.edn/read-string (slurp path)))
 
 (defn help []
   (println (:out (apply clojure.java.shell/sh ["swap-cli" "help"]))))
 
 (comment (help))
 
-(defn offer-take [swap-index]
+(defn offer-take [swap-index {:keys [address-btc address-xmr data-dir-root]}]
   (let [result (apply clojure.java.shell/sh ["~/.cargo/bin/swap-cli"
-                                            "-d" (str master-data-dir ".data_dir_" swap-index)
+                                            "-d" (str data-dir-root ".data_dir_" swap-index)
                                             "take" "-w"
-                                            "--btc-addr" farcaster-stash-btc
-                                            "--xmr-addr" farcaster-stash-xmr
+                                            "--btc-addr" address-btc
+                                            "--xmr-addr" address-xmr
                                             "--offer" (nth @offers (mod swap-index (count @offers)))])]
     (println [(:out result) (:err result)])
     ))
@@ -49,9 +47,10 @@
 (def simple (atom 0))
 (comment (reset! simple 200))
 
-(comment (let [count 20]
+(comment (let [count 20
+               config (read-config "config.edn")]
            (reset! offers (doall (offers-get)))
-           (doall (map offer-take (range @simple (+ @simple count))))
+           (doall (map #(offer-take % config) (range @simple (+ @simple count))))
            (reset! simple (+ count @simple))
            ;; (reset! offers-bag @offers)
            (println @simple)
@@ -63,11 +62,13 @@
 
 (defn -main [& args]
   (if (= (count args) 2)
-    (let [[min-swap-index max-swap-index] (map #(Integer/parseInt %) args)]
+    (let [[min-swap-index max-swap-index] (map #(Integer/parseInt %) args)
+          config (read-config "config.edn")]
       (println "swap index range: " min-swap-index max-swap-index)
+      (println "config: " config)
       (reset! offers (offers-get))
       ;; (println "offers: " @offers)
-      (map offer-take (range min-swap-index (max (inc max-swap-index) (+ min-swap-index (count @offers)))))
+      (map #(offer-take % config) (range min-swap-index (max (inc max-swap-index) (+ min-swap-index (count @offers)))))
       )
     (println "required args: min-swap-index max-swap-index"))
   )
