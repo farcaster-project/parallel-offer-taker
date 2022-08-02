c19be8af647580f6a0118a4ba590ebc6c55aaef6
Robert Hambrock <roberthambrock@gmail.com>
2022-08-02 10:22:29 +0200

    add farcasterd launcher & killer

--- src/parallel_offer_taker/core.clj
+++ src/parallel_offer_taker/core.clj
@@ -5,8 +5,9 @@
    [clojure.data.json :as json]
    clojure.edn
    clojure.java.io
-   [clojure.java.shell]
-   clojure.set)
+   [clojure.java.shell :as shell]
+   clojure.set
+   clojure.string)
   (:gen-class)
   )
 
@@ -30,18 +31,41 @@
 (defn read-config [path]
   (clojure.edn/read-string (slurp path)))
 
+(defn add-missing-trailing-slash [path]
+  (if (and path (not= \/ (last path)))
+    (str path "/")
+    path))
+
+(defn data-dir [swap-index {:keys [data-dir-root]}]
+  (let [data-dir (str (add-missing-trailing-slash data-dir-root) ".data_dir_" swap-index)]
+    (try
+      (if (.exists (clojure.java.io/as-file data-dir))
+        data-dir
+        (throw (Exception. (str data-dir " does not exist"))))
+      (catch Exception e
+        (println e)))))
+
+(defn binary-dir [config]
+  (or (add-missing-trailing-slash (:farcaster-binaries-path config)) "~/.cargo/bin/"))
+
+(defn binary-path [config binary-name]
+  (str (binary-dir config) binary-name))
+
+(defn farcaster-config-toml-file [config]
+  (or (:farcaster-config-toml-file config) "~/.farcaster/farcaster.toml"))
+
 (defn help []
-  (println (:out (apply clojure.java.shell/sh ["swap-cli" "help"]))))
+  (println (:out (apply shell/sh ["swap-cli" "help"]))))
 
 (comment (help))
 
-(defn offer-take [swap-index {:keys [address-btc address-xmr data-dir-root]}]
-  (let [result (apply clojure.java.shell/sh ["~/.cargo/bin/swap-cli"
-                                            "-d" (str data-dir-root ".data_dir_" swap-index)
-                                            "take" "-w"
-                                            "--btc-addr" address-btc
-                                            "--xmr-addr" address-xmr
-                                            "--offer" (nth @offers (mod swap-index (count @offers)))])]
+(defn offer-take [swap-index config]
+  (let [result (apply shell/sh [(binary-path config "swap-cli")
+                                "-d" (data-dir config swap-index)
+                                "take" "-w"
+                                "--btc-addr" (:address-btc config)
+                                "--xmr-addr" (:address-xmr config)
+                                "--offer" (nth @offers (mod swap-index (count @offers)))])]
     (println [(:out result) (:err result)])
     ))
 
@@ -61,26 +85,9 @@
   (update-offers)
   (pmap offer-take (range 20)))
 
-(defn add-missing-trailing-slash [path]
-  (if (not= \/ (last path))
-    (str path "/")
-    path))
-
-(defn data-dir [swap-index {:keys [data-dir-root]}]
-  (let [data-dir (str (add-missing-trailing-slash data-dir-root) ".data_dir_" swap-index)]
-    (try
-      (if (.exists (clojure.java.io/as-file data-dir))
-        data-dir
-        (throw (Exception. (str data-dir " does not exist"))))
-      (catch Exception e
-        (println e)))))
-
-(defn binary-dir [config]
-  (or (add-missing-trailing-slash (:farcaster-binaries-path config)) "***REMOVED***"))
-
-(defn syncer-running? [swap-index config]
+(defn farcasterd-running? [swap-index config]
   (let [data-dir (data-dir swap-index config)]
-    (if data-dir (try (-> (apply clojure.java.shell/sh
+    (if data-dir (try (-> (apply shell/sh
                                  [
                                   (str (binary-dir config) "swap-cli")
                                   "-d" data-dir
@@ -93,15 +100,68 @@
                         false))
         false)))
 
+(farcasterd-running? 0 (read-config "config.edn"))
+
+(defn farcasterd-launch-vec [swap-index config]
+  (let [data-dir (data-dir swap-index config)]
+    [
+     (str (binary-dir config) "farcasterd")
+     "-c" (farcaster-config-toml-file config)
+     "-d" data-dir
+     ]))
+
+(def process-futures (atom {}))
+
+(defn launch-farcasterd [swap-index config]
+  (->> config
+       (farcasterd-launch-vec swap-index)
+       (apply shell/sh)
+       future
+       ;; NOTE: this still requires check that a future does not exist under given key yet
+       ((fn [future] (swap! process-futures (fn [m] (assoc m swap-index future)))))))
+
+(defn farcasterd-process-id-exact [swap-index config]
+  (->> ["bash" "-c" (str "ps -ef | grep \"" (clojure.string/join " " (farcasterd-launch-vec swap-index config)) "$\" | grep -v grep | awk '{print $2}'"
+                         )]
+       (apply shell/sh)
+       :out
+       clojure.string/trim-newline
+       Integer/parseInt))
+
+(defn farcasterd-process-id [swap-index]
+  (->> ["bash" "-c" (str "ps -ef | grep farcasterd | grep .data_dir_" swap-index "$ | grep -v grep | awk '{print $2}'"
+                         )]
+       (apply shell/sh)
+       :out
+       clojure.string/trim-newline
+       Integer/parseInt))
+
+(comment
+  (farcasterd-process-id-exact 0 (read-config "config.edn"))
+  (farcasterd-process-id 0))
+
+(defn kill-farcasterd [swap-index config]
+  (let [process-id (or (farcasterd-process-id-exact swap-index config)
+                       (farcasterd-process-id swap-index))]
+    (if process-id
+      (shell/sh "kill" "-s" "SIGTERM" (str process-id))
+      (throw (Exception. (str "no process for swap-index " swap-index))))))
+
 (defn -main [& args]
   (if (= (count args) 2)
     (let [[min-swap-index max-swap-index] (map #(Integer/parseInt %) args)
-          config (read-config "config.edn")]
+          config (read-config "config.edn")
+          unresponsive-daemons (filter #(not (farcasterd-running? % config)) (range min-swap-index max-swap-index))]
       (println "swap index range: " min-swap-index max-swap-index)
       (println "config: " config)
-      (reset! offers (offers-get))
-      ;; (println "offers: " @offers)
-      (map #(offer-take % config) (range min-swap-index (max (inc max-swap-index) (+ min-swap-index (count @offers)))))
+      (if (empty? unresponsive-daemons)
+        (do
+          (println "all running - can continue!")
+          ;; (reset! offers (offers-get))
+          ;; (println "offers: " @offers)
+          ;; (map #(offer-take % config) (range min-swap-index (max (inc max-swap-index) (+ min-swap-index (count @offers)))))
+          )
+        (println "following daemons aren't responding, please get them running first:" unresponsive-daemons))
       )
     (println "required args: min-swap-index max-swap-index"))
   )
