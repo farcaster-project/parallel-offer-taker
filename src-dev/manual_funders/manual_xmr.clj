80962f55881a12e35ed3f00848fdb877c8cee784
Robert Hambrock <roberthambrock@gmail.com>
2022-08-15 13:23:26 +0200

    recoerce to bigInt

--- src-dev/manual_funders/manual_xmr.clj
+++ src-dev/manual_funders/manual_xmr.clj
@@ -8,12 +8,12 @@
 (def destination-file (atom "default-xmr"))
 
 (defn fixed-float-to-big-int [ff]
-  ((fn [[head tail]] (+ (* (bigint head) 1E12)
-                       (bigint (apply str tail (repeat (- 12 (count tail)) "0")))))
+  ((fn [[head tail]] (bigint (+ (* (bigint head) 1E12)
+                               (bigint (apply str tail (repeat (- 12 (count tail)) "0"))))))
    (clojure.string/split ff #"\.")))
 
 (comment
-  (= 3.2358865E11
+  (= 323588650000
      (fixed-float-to-big-int "0.323588650000")
      (fixed-float-to-big-int "0.32358865000")
      (fixed-float-to-big-int "0.32358865")))
